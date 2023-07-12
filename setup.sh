89cb6109b2cda28c792d0562c10d6d7836c5a43a
Stefano Di Pasquale <stefaniscion@gmail.com>
2023-07-12 20:43:57 +0200

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,26 @@
+# DASHBOARD TOOLS
+# cockpit
 echo "* installing cockpit"
 sudo dnf install cockpit -y
 sudo systemctl enable --now cockpit.socket
-#sudo firewall-cmd --add-service=cockpit --permanent
-#sudo firewall-cmd --reload
+sudo firewall-cmd --add-service=cockpit --permanent
+sudo firewall-cmd --reload
+
+# FILESYSTEM TOOLS
+# mergerfs
+echo "* installing mergerfs"
+sudo dnf install mergerfs -y
+# snapraid
+echo "* installing snapraid"
+sudo dnf install snapraid -y
 echo "* done!"
+
+# CONTAINERS TOOLS
+# podman
+echo "* installing cockpit"
+sudo dnf install podman -y
+# docker-compose
+echo "* installing docker-compose"
+sudo dnf install docker-compose -y
+
 #sudo reboot
