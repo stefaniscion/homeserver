ccd27dffa3c81a512d51f09df0565021d450380c
Stefano Di Pasquale <stefaniscion@gmail.com>
2023-12-07 23:05:59 +0100

    added swag, .env edits

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 docker compose -f services/duckdns.yml up -d
+docker compose -f services/swag.yml up -d
 docker compose -f services/nextcloud.yml up -d
 docker compose -f services/jellyfin.yml up -d
