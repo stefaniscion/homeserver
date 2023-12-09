0ed3ebd21ecc2e932bc74e048057d097a1596ed8
Stefano Di Pasquale <stefaniscion@gmail.com>
2023-12-09 17:33:11 +0100

    added stop.sh

---
+++ stop.sh
@@ -0,0 +1,5 @@
+docker compose -f services/duckdns.yml down
+docker compose -f services/swag.yml down
+docker compose -f services/nextcloud.yml down
+docker compose -f services/jellyfin.yml down
+docker compose -f services/homeassistant.yml down
