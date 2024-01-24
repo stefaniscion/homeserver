6e87467c0d8c2279cb7ae3522415a586f30e6264
Stefano Di Pasquale <stefaniscion@gmail.com>
2024-01-25 00:43:16 +0100

    updated setup.sh for archlinux

--- setup.sh
+++ setup.sh
@@ -1,46 +1,34 @@
-# UPDATE SYSTEM
-echo "***** updating system"
-sudo dnf update -y
-echo "***** installing epel repos"
-sudo dnf config-manager --set-enabled crb
-sudo dnf install \
-    https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm \
-    https://dl.fedoraproject.org/pub/epel/epel-next-release-latest-9.noarch.rpm
+read -p "Do you want to install the Stefaniscion's Homeserver? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "***** updating system"
+    sudo pacman -Syu --noconfirm
 
-# UTILS
-sudo dnf install wget -y
+    echo "***** installing utility packages"
+    sudo pacman -Sy wget htop --noconfirm
+    sudo pacman -S --needed base-devel --noconfirm
 
-## DASHBOARD TOOLS
-#echo "***** installing cockpit"
-#sudo dnf install cockpit -y
-#sudo systemctl enable --now cockpit.socket
-#sudo firewall-cmd --add-service=cockpit --permanent
-#sudo firewall-cmd --reload
-#sudo dnf install cockpit-storaged cockpit-networkmanager cockpit-packagekit cockpit-podman cockpit-selinux cockpit-sosreport  -y
+    echo "***** installing yay"
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si  --noconfirm
+    cd ..
+    rm -rf yay
 
-# FILESYSTEM TOOLS
-echo "***** installing mergerfs"
-sudo dnf install fuse -y
-wget https://github.com/trapexit/mergerfs/releases/download/2.35.1/mergerfs-2.35.1-1.fc33.aarch64.rpm
-sudo rpm -i mergerfs-2.35.1-1.fc33.aarch64.rpm
-sudo rm mergerfs-2.35.1-1.fc33.aarch64.rpm
-echo "***** installing snapraid"
-sudo dnf install snapraid
+    echo "***** installing mergerfs"
+    yay -Sy mergerfs fuse2 --noconfirm
 
-# CONTAINERS TOOLS
-sudo dnf install -y yum-utils
-sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo systemctl --now enable docker
-sudo groupadd docker
-sudo usermod -aG docker $USER
+    echo "***** installing snapraid"
+    yay -Sy snapraid --noconfirm
+
+    echo "***** installing docker"
+    sudo pacman -Sy docker docker-compose --noconfirm
+    sudo usermod -aG docker $USER
+    sudo systemctl enable docker.service
+    
+    echo "***** Rebooting"
+    sudo reboot
+fi
 
 
-#echo "***** installing podman"
-#sudo dnf install podman -y
-#echo "***** installing podman-compose"
-#sudo dnf install podman-compose -y
-
-# FINALIZING
-echo "***** done!"
-#sudo reboot
