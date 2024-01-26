13ce0961eef454aebf88480d3805a2e69db636f6
Stefano Di Pasquale <stefaniscion@gmail.com>
2024-01-26 18:51:34 +0100

    implemented logging

--- manteniance-scripts/lib/clean_config_backup_directory.py
+++ manteniance-scripts/lib/clean_config_backup_directory.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 def clean_config_backup_directory(config_backup_path):
     max_files = 10
@@ -7,14 +8,15 @@ def clean_config_backup_directory(config_backup_path):
     # if there are more than x files, delete the oldest one
     if len(files) > max_files:
         files.sort()
-        print(files)
         # get the list of files to delete
         files_to_delete = []
         num_files_to_delete = len(files)-max_files
         files_to_delete = files[:num_files_to_delete]
-        print(files_to_delete)
+        logging.info("- Files to delete: "+file_to_delete)
         #delete the files
         for file_to_delete in files_to_delete:
             file_to_delete_path = os.path.join(config_backup_path,file_to_delete)
             os.remove(file_to_delete_path)
-            print("- Deleted: "+file_to_delete)
+            logging.info("- Deleted: "+file_to_delete)
+    else:
+        logging.info("- No files to delete.")
--- manteniance-scripts/main.py
+++ manteniance-scripts/main.py
@@ -1,7 +1,11 @@
 import os
+import logging
 from dotenv import dotenv_values
 from lib.make_config_backup import make_config_backup
 from lib.clean_config_backup_directory import clean_config_backup_directory
+
+# set up logging
+logging.basicConfig(level=logging.INFO)
 
 # get the config path from the .env file
 config = dotenv_values("./services/.env")
@@ -10,8 +14,8 @@ config = dotenv_values("./services/.env")
 config_path = config["CONFIG_PATH"]
 config_backup_path = os.path.join(config["STORAGE_PATH"]+"/config_bak/")
 
-print("* Making config backup...")
+logging.info("* Making config backup...")
 make_config_backup(config_path,config_backup_path)
 
-print("* Cleaning config backup directory...")
+logging.info("* Cleaning config backup directory...")
 clean_config_backup_directory(config_backup_path)
