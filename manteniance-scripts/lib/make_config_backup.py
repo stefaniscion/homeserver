70f2237e3af52ef5e8a17b702b4a6362d525abb1
Stefano Di Pasquale <stefaniscion@gmail.com>
2024-01-26 18:23:14 +0100

    changed naming conventions of backup archive

--- manteniance-scripts/lib/make_config_backup.py
+++ manteniance-scripts/lib/make_config_backup.py
@@ -1,13 +1,14 @@
 import os
 import tarfile
-from pathlib import Path
+from datetime import datetime
 
 def make_config_backup(config_path,config_backup_path):
     # if the backup directory doesn't exist, create it
     if not os.path.isdir(config_backup_path):
         os.mkdir(config_backup_path)
+    # assemble the archive name with the current date
+    now_string = datetime.now().strftime("%Y%m%d%I%M%S")
+    archive_name = "config_bak_"+now_string+".tar.xz"
     # archive the config directory in a .tar.xz
-    tar_file_path = Path(os.path.join(config_backup_path, "config.tar.xz"))
-    config_path = Path(config_path)
-    with tarfile.open(tar_file_path, "w:xz") as tar:
-        tar.add(config_path, arcname=config_path.stem)
+    with tarfile.open(os.path.join(config_backup_path,archive_name), "w:xz") as tar:
+        tar.add(config_path)
