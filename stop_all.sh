96a12b5fa88c85f20ca83694108b902b02de3e08
Stefano Di Pasquale <stefaniscion@gmail.com>
2024-01-26 15:51:08 +0100

    corrected homeassistnt path

--- stop_all.sh
+++
@@ -1,5 +0,0 @@
-docker compose -f services/duckdns/duckdns.yml down
-docker compose -f services/swag/swag.yml down
-docker compose -f services/nextcloud/nextcloud.yml down
-docker compose -f services/jellyfin/jellyfin.yml down
-docker compose -f services/homeassistant/homeassistant.yml down
