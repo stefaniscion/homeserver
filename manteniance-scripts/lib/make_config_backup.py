ded001b6cdc39151ba2f4d490e41287c5b142584
Stefano Di Pasquale <stefaniscion@gmail.com>
2024-01-26 17:38:22 +0100

    solved bug where directory is missing

--- manteniance-scripts/lib/make_config_backup.py
+++ manteniance-scripts/lib/make_config_backup.py
@@ -1,8 +1,11 @@
+import os
 import tarfile
-from os.path import join
 
 def make_config_backup(config_path,config_backup_path):
+    # if the backup directory doesn't exist, create it
+    if not os.path.isdir(config_backup_path):
+        os.mkdir(config_backup_path)
     # archive the config directory in a .tar.xz
-    tar_file_path = join(config_backup_path, "config.tar.xz")
+    tar_file_path = os.path.join(config_backup_path, "config.tar.xz")
     with tarfile.open(tar_file_path, "w:xz") as tar:
         tar.add(config_path, arcname=config_path.stem)
