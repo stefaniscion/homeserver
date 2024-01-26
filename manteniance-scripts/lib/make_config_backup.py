bec81c17adf7f99453a1a967415b71f097a916b1
Stefano Di Pasquale <stefaniscion@gmail.com>
2024-01-26 19:03:48 +0100

    linted

--- manteniance-scripts/lib/make_config_backup.py
+++ manteniance-scripts/lib/make_config_backup.py
@@ -1,9 +1,9 @@
 import os
-import logging
 import tarfile
 from datetime import datetime
 
-def make_config_backup(config_path,config_backup_path):
+
+def make_config_backup(config_path, config_backup_path):
     # if the backup directory doesn't exist, create it
     if not os.path.isdir(config_backup_path):
         os.mkdir(config_backup_path)
@@ -11,5 +11,5 @@ def make_config_backup(config_path,config_backup_path):
     now_string = datetime.now().strftime("%Y%m%d%I%M%S")
     archive_name = "config_bak_"+now_string+".tar.xz"
     # archive the config directory in a .tar.xz
-    with tarfile.open(os.path.join(config_backup_path,archive_name), "w:xz") as tar:
+    with tarfile.open(os.path.join(config_backup_path, archive_name), "w:xz") as tar:
         tar.add(config_path)
