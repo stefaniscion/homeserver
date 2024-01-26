65605222fe2850775af264c6a7b2bd5601cdee6c
Stefano Di Pasquale <stefaniscion@gmail.com>
2024-01-26 19:03:42 +0100

    fixed wrong var name, linted

--- manteniance-scripts/lib/clean_config_backup_directory.py
+++ manteniance-scripts/lib/clean_config_backup_directory.py
@@ -1,6 +1,7 @@
 import os
 import logging
 
+
 def clean_config_backup_directory(config_backup_path):
     max_files = 10
     # get the list of files in the backup directory
@@ -12,10 +13,10 @@ def clean_config_backup_directory(config_backup_path):
         files_to_delete = []
         num_files_to_delete = len(files)-max_files
         files_to_delete = files[:num_files_to_delete]
-        logging.info("- Files to delete: "+file_to_delete)
+        logging.info("- Files to delete: "+files_to_delete)
         #delete the files
         for file_to_delete in files_to_delete:
-            file_to_delete_path = os.path.join(config_backup_path,file_to_delete)
+            file_to_delete_path = os.path.join(config_backup_path, file_to_delete)
             os.remove(file_to_delete_path)
             logging.info("- Deleted: "+file_to_delete)
     else:
