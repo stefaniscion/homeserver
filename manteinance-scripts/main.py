779384697c90b331ef4b24376fc2438aab1dadea
Stefano Di Pasquale <stefaniscion@gmail.com>
2024-06-11 20:16:27 +0200

    fixed logging

--- manteinance-scripts/main.py
+++ manteinance-scripts/main.py
@@ -7,16 +7,36 @@ from lib.clean_config_backup_directory import clean_config_backup_directory
 from lib.snapraid_scrub import snapraid_scrub
 from lib.snapraid_sync import snapraid_sync
 
+# get the config path from the .env file
+config = dotenv_values(".env")
+
+# get some paths
+config_path = config["CONFIG_PATH"]
+config_backup_path = os.path.join(config["STORAGE_PATH"] + "/config_bak/")
+logging_path = os.path.join(config["LOGGING_PATH"] + "/manteniance.log")
 
 # set up logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+    handlers=[logging.FileHandler(logging_path), logging.StreamHandler()],
+)
+
 
 # parse arguments
-parser = argparse.ArgumentParser(description='Run some maintenance operations.')
-parser.add_argument('--configbackup', action=argparse.BooleanOptionalAction, default=True)
-parser.add_argument('--cleanbackup', action=argparse.BooleanOptionalAction, default=True)
-parser.add_argument('--snapraidscrub', action=argparse.BooleanOptionalAction, default=True)
-parser.add_argument('--snapraidsync', action=argparse.BooleanOptionalAction, default=True)
+parser = argparse.ArgumentParser(description="Run some maintenance operations.")
+parser.add_argument(
+    "--configbackup", action=argparse.BooleanOptionalAction, default=True
+)
+parser.add_argument(
+    "--cleanbackup", action=argparse.BooleanOptionalAction, default=True
+)
+parser.add_argument(
+    "--snapraidscrub", action=argparse.BooleanOptionalAction, default=True
+)
+parser.add_argument(
+    "--snapraidsync", action=argparse.BooleanOptionalAction, default=True
+)
 args = parser.parse_args()
 
 # valorize things to do
@@ -25,12 +45,6 @@ do_clean_backup = args.cleanbackup
 do_snapraid_scrub = args.snapraidscrub
 do_snapraid_sync = args.snapraidsync
 
-# get the config path from the .env file
-config = dotenv_values("./services/.env")
-
-# get some paths
-config_path = config["CONFIG_PATH"]
-config_backup_path = os.path.join(config["STORAGE_PATH"] + "/config_bak/")
 
 # run the operations
 
