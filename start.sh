2fad1217862f2a31125a885f47401558a8fc303a
Stefano Di Pasquale <stefaniscion@gmail.com>
2025-04-30 16:59:39 +0200

    correct path in start/stop sh scripts

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker compose -f services/docker-compose.yml up -d
+docker compose -f docker-compose.yml up -d
--- stop.sh
+++ stop.sh
@@ -1,1 +1,1 @@
-docker compose -f services/docker-compose.yml down
+docker compose -f docker-compose.yml down
